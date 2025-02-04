c1c8bf1bb080ce51cdd98097c9b599cb418c52d7
Niklas Kolmer <antonniki75@gmail.com>
2025-02-04 09:27:23 +0100

    Initial Commit

--- php/process.php
+++ php/process.php
@@ -1,1 +1,6 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    echo "Hallo, " . $name . "! Deine Anfrage wurde erfolgreich verarbeitet.";
+}
+?>
