1998170cf7a2e0ec6377e2c92dd20112b9312084
Niklas <antonniki75@gmail.com>
2025-02-09 01:32:57 +0100

    register-UI

--- public/js/startseite1.js
+++ public/js/startseite1.js
@@ -1,10 +1,39 @@
 function login(action){
-    console.log('testtezudfius');
     let popup = document.getElementById("popuplogin");
 
     if (action === "show") {
         popup.style.display = "block";
     } else if (action === "hide") {
         popup.style.display = "none";
+    }
+}
+
+function register(action){
+    let popup = document.getElementById("popupregister");
+
+    if (action === "show") {
+        popup.style.display = "block";
+    } else if (action === "hide") {
+        popup.style.display = "none";
+    }
+}
+
+function changeGeschlecht(action) {
+    let maennlich = document.getElementById('maennlich');
+    let weiblich = document.getElementById('weiblich');
+    let divers = document.getElementById('divers');
+
+    if(action === 'm'){
+        maennlich.checked = true;
+        weiblich.checked = false;
+        divers.checked = false;
+    }else if(action === 'w'){
+        maennlich.checked = false;
+        weiblich.checked = true;
+        divers.checked = false;
+    }else if(action === 'd'){
+        maennlich.checked = false;
+        weiblich.checked = false;
+        divers.checked = true;
     }
 }
