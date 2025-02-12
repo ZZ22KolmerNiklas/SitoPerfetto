c18a4d8fad09ca7375ce94cbd737030ab592fffe
Dave <david.pititto@bs-erlangen.de>
2025-02-12 11:18:41 +0100

    vorbereitung pdf erstellung

--- public/js/verwaltung1.js
+++ public/js/verwaltung1.js
@@ -32,7 +32,7 @@ window.onload = function(){
                 const tr = document.createElement("tr");
                 tr.innerHTML = `<td>${row.zimmernummer}</td><td>${row.vorname}</td><td>${row.nachname}</td><td>${row.von}</td><td>${row.bis}</td>
                                     <td>${row.email}</td><td>${row.anzahlBetten}</td><td>${gesamtPreis}€</td><td>${row.stammkunde}</td>
-                                    <td><button onclick="test()">Erstellen</button></td><td><button onclick="test()">Bearbeiten</button></td>`;
+                                    <td><button onclick="generateInvoice()">Erstellen</button></td><td><button onclick="test()">Bearbeiten</button></td>`;
                 tableBody.appendChild(tr);
             });
         })
@@ -43,34 +43,51 @@ window.onload = function(){
         document.getElementById('name').innerText = sessionStorage.getItem("username");
     }
 }
+function preisVerwalten(action, senden) {
+    let verwaltung = document.getElementById("preisVerwaltungPopup");
+    if (action === 'show') {
+        verwaltung.style.display = "block";
+    } else if (action === 'hide') {
+        verwaltung.style.display = "none";
+    }
+    if (senden === "senden") {
+            /*an DB übergeben*/
+    }
+}
+
 async function generateInvoice() {
     const {jsPDF} = window.jspdf;
     const doc = new jsPDF();
 
     doc.text("FUNREST Hotel", 10, 10);
     doc.text("Funstraße 1, 12345 Berlin", 10, 20);
-    doc.text("RechnungNr: ___", 10, 40);
-    doc.text("Von: __/__/____", 10, 50);
-    doc.text("Bis: __/__/____", 10, 60);
-    doc.text("Zeitraum: ___ Nächte", 10, 70);
-    doc.text("Zimmerart: 'Luxus'", 10, 80);
-    doc.text("Zimmernummer: '301'", 10, 90);
-    doc.text("Preis: ___€", 10, 100);
+    doc.text("RechnungNr: ", 10, 40);
+    doc.text("BuchungsNr: ___", 10, 50)
+    doc.text("Von: __/__/____", 10, 60);
+    doc.text("Bis: __/__/____", 10, 70);
+    doc.text("Zeitraum: ___ Nächte", 10, 80);
+    doc.text("Zimmerart: 'Luxus'", 10, 90);
+    doc.text("Zimmernummer: '301'", 10, 100);
+    doc.text("Preis: ___€", 10, 110);
+
+    doc.save("download/rechnung.pdf");
 
     const pdfData = doc.output("blob");
 
     const formData = new FormData();
     formData.append("file", pdfData, "rechnung.pdf");
-}
 
-    function preisVerwalten(action, senden) {
-        let verwaltung = document.getElementById("preisVerwaltungPopup");
-        if (action === 'show') {
-            verwaltung.style.display = "block";
-        } else if (action === 'hide') {
-            verwaltung.style.display = "none";
+    try {
+        const response = await fetch("../../php/rechnung", {
+            method: "POST",
+            body: formData
+        });
+        if (response.ok) {
+            alert("Rechnung wurde erfolgreich gespeichert!");
+        } else {
+            alert("Fehler beim Speichern der Rechnung.");
         }
-        if (senden === "senden") {
-            /*an DB übergeben*/
-        }
+    } catch (error) {
+        alert("Netzwerkfehler beim Speichern der Rechnung.");
     }
+}
