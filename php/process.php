a4f2b0ad7bcbaefca6f0a87a1b1d8a5173b62138
Niklas <antonniki75@gmail.com>
2025-03-20 09:00:31 +0100

    Refactor

--- php/process.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST["name"]);
-    echo "Hallo, " . $name . "! Deine Anfrage wurde erfolgreich verarbeitet.";
-}
-?>
